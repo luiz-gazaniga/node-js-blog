e7bd86458c8b554e700b9e2470313b12bfef3be0
Luiz Augusto Gazaniga <luiz.gazaniga@gmail.com>
2023-05-18 00:06:35 -0300

    text editor.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 
+const edge = require('edge.js')
+
 const expressEdge = require('express-edge')
 
 const mongoose = require('mongoose')
 
 const bodyParser = require('body-parser');
 
-const validateCratePostMiddleware = require('./middleware/storePostMIddleware');
+const validateCratePostMiddleware = require('./middleware/storePostMiddleware');
 const auth = require('./middleware/auth');
+const redirectIfAuthenticatedMiddleware = require('./middleware/redirectIfAuthenticatedMiddleware');
 
 const fileUpload = require('express-fileupload');
 const expressSession = require('express-session');
@@ -25,6 +28,8 @@ const storeUserController = require('./controllers/storeUserController');
 
 const loginController = require('./controllers/loginController');
 const loginUserController = require('./controllers/loginUserController');
+
+const logoutController = require('./controllers/logoutController');
 
 const app = new express();
 
@@ -50,6 +55,12 @@ app.use(expressEdge)
 
 app.set('views', `${__dirname}/views`)
 
+app.use('*', (req, res, next) => {
+    res.locals.auth = req.session.userId;
+
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -61,11 +72,15 @@ app.get('/posts/new', auth, createPostController);
 app.post('/posts/store', auth, validateCratePostMiddleware, storePost);
 app.get('/posts/:id', getPost);
 
-app.get('/auth/register', createUserController);
-app.post('/users/register', storeUserController);
+app.get('/auth/register', redirectIfAuthenticatedMiddleware, createUserController);
+app.post('/users/register', redirectIfAuthenticatedMiddleware, storeUserController);
 
-app.get('/auth/login', loginController);
-app.post('/users/login', loginUserController);
+app.get('/auth/login', redirectIfAuthenticatedMiddleware, loginController);
+app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController);
+
+app.get('/auth/logout', auth, logoutController);
+
+app.use((_req, res) => res.render('not-found'));
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
