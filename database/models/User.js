90d0656b72b00aca53ddf2ea03572198a7cfb8ce
Luiz Augusto Gazaniga <luiz.gazaniga@gmail.com>
2023-05-18 23:29:44 -0300

    refactor User.

--- database/models/User.js
+++ database/models/User.js
@@ -1,34 +1,42 @@
 const bcrypt = require('bcrypt');
-
 const mongoose = require('mongoose');
 
-const Userschema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'Please provide your username']
-    },
-    email: {
-        type: String,
-        required: [true, 'Please provide your email'],
-        unique: true
-    },
-    password: { 
-        type: String,
-        required: [true, 'Please provide your password']
-    },
-    createdAt: {
-        type: Date,
-        default: new Date()
-    }
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide your username'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide your password'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-Userschema.pre('save', async function(next){
-    const user = this;
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
 
-    user.password = await bcrypt.hash(user.password, 10);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
     next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
-const User = mongoose.model('User', Userschema)
+const User = mongoose.model('User', UserSchema);
 
-module.exports = User
+module.exports = User;
